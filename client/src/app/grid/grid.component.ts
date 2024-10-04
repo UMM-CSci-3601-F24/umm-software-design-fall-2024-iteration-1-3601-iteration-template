d08860066d57e8bb040cced4e0e48808e230be8b
JosieBarber <barbe497@umn.edu>
2024-10-03 22:39:57 -0500

    Start Implementing GridComponent with fixed Cell

--- client/src/app/grid/grid.component.ts
+++ client/src/app/grid/grid.component.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterLink } from '@angular/router';
+import { AsyncPipe, CommonModule } from '@angular/common';
+
+import { GridCellComponent } from '../grid-cell/grid-cell.component';
+
+
 @Component({
-  selector: 'app-grid',
-  standalone: true,
-  imports: [],
+  selector: 'app-grid-component',
   templateUrl: './grid.component.html',
-  styleUrl: './grid.component.scss'
+  styleUrls: ['./grid.component.scss'],
+  standalone: true,
+  providers: [],
+  imports: [
+    AsyncPipe,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatRadioModule,
+    MatListModule,
+    RouterLink,
+    MatButtonModule,
+    MatTooltipModule,
+    MatIconModule,
+    CommonModule,
+    GridCellComponent
+  ],
 })
 export class GridComponent {
 
+  gridRows: number[];
+  gridColumns: number[];
+
+  constructor() {
+    this.gridRows = Array.from({ length: 5 }, (_, index) => index);
+    this.gridColumns = Array.from({ length: 5 }, (_, index) => index);
+  }
 }
