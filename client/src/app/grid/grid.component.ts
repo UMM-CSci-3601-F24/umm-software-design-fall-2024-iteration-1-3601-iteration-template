a505626b926bbf5b8819297d8617129ec7ed5d46
Josie Barber <barbe497@umn.edu>
2024-10-09 16:06:05 -0500

    Testing & Cleanup

--- client/src/app/grid/grid.component.ts
+++ client/src/app/grid/grid.component.ts
@@ -17,7 +17,6 @@ import { AsyncPipe, CommonModule } from '@angular/common';
 import { GridCell } from '../grid-cell/grid-cell';
 import { GridCellComponent } from '../grid-cell/grid-cell.component';
 
-
 @Component({
   selector: 'app-grid-component',
   templateUrl: 'grid.component.html',
@@ -25,19 +24,9 @@ import { GridCellComponent } from '../grid-cell/grid-cell.component';
   standalone: true,
   providers: [],
   imports: [
-    AsyncPipe,
-    MatCardModule,
     MatFormFieldModule,
     MatInputModule,
     FormsModule,
-    MatSelectModule,
-    MatOptionModule,
-    MatRadioModule,
-    MatListModule,
-    RouterLink,
-    MatButtonModule,
-    MatTooltipModule,
-    MatIconModule,
     CommonModule,
     GridCellComponent,
     MatGridListModule,
@@ -53,7 +42,7 @@ export class GridComponent {
   typingDirections: string[] = ["right", "left", "up", "down"];
   currentDirectionIndex: number = 0;
 
-  constructor(private renderer: Renderer2, private elRef: ElementRef) {
+  constructor(private renderer: Renderer2, public elRef: ElementRef) {
     this.initializeGrid();
   }
 
@@ -72,9 +61,6 @@ export class GridComponent {
    }
   }
 
-
-
-
   onClick(event: MouseEvent, col: number, row: number) {
     this.moveFocus(col, row);
   }
@@ -82,6 +68,8 @@ export class GridComponent {
   onKeydown(event: KeyboardEvent, row: number, col: number) {
     const cell = this.grid[col][row];
     const inputElement = this.elRef.nativeElement.querySelector(`app-grid-cell[data-col="${col}"][data-row="${row}"] input`);
+
+    console.log('keydown', event.key, col, row);
 
     if (!event.ctrlKey) {
       switch (event.key) {
@@ -98,53 +86,68 @@ export class GridComponent {
             this.moveFocus(col + 1, row);
             break;
           case 'Backspace':
-            cell.value = event.key;
             if (inputElement) {
-              this.renderer.setProperty(inputElement, 'value', '');
+              // this.renderer.setProperty(inputElement, 'value', '');
+              cell.value = '';
             }
             if (this.typeDirection === "right") {
-              setTimeout(() => this.moveFocus(col - 1, row), 0);
+              // setTimeout(() => this.moveFocus(col - 1, row), 0);
+              this.moveFocus(col - 1, row)
             }
             if (this.typeDirection === "left") {
-              setTimeout(() => this.moveFocus(col + 1, row), 0);
+              // setTimeout(() => this.moveFocus(col + 1, row), 0);
+              this.moveFocus(col + 1, row)
             }
             if (this.typeDirection === "up") {
-              setTimeout(() => this.moveFocus(col, row + 1), 0);
+              // setTimeout(() => this.moveFocus(col, row + 1), 0);
+              this.moveFocus(col, row + 1)
             }
             if (this.typeDirection === "down") {
-              setTimeout(() => this.moveFocus(col, row - 1), 0);
+              // setTimeout(() => this.moveFocus(col, row - 1), 0);
+              this.moveFocus(col, row - 1)
             }
             break;
           default:
             if (event.key.length === 1 && event.key.match(/[a-zA-Z]/)) {
+              console.log('old cell value = ', cell.value);
               cell.value = event.key;
-              if (inputElement) {
-                this.renderer.setProperty(inputElement, 'value', event.key);
-              }
+              console.log('new cell value = ', cell.value);
+              // if (inputElement) {
+              //   // this.renderer.setProperty(inputElement, 'value', event.key);
+              //   console.log(cell, ' at ', col, row);
+              //   cell.value = event.key;
+              // }
               if (this.typeDirection === "right") {
-                setTimeout(() => this.moveFocus(col + 1, row), 0);
+                // setTimeout(() => this.moveFocus(col + 1, row), 0);
+                console.log('moving focus to ', col + 1, row);
+                this.moveFocus(col + 1, row)
               }
               if (this.typeDirection === "left") {
-                setTimeout(() => this.moveFocus(col - 1, row), 0);
+                // setTimeout(() => this.moveFocus(col - 1, row), 0);
+                this.moveFocus(col - 1, row)
               }
               if (this.typeDirection === "up") {
-                setTimeout(() => this.moveFocus(col, row - 1), 0);
+                // setTimeout(() => this.moveFocus(col, row - 1), 0);
+                this.moveFocus(col, row - 1)
+
               }
               if (this.typeDirection === "down") {
-                setTimeout(() => this.moveFocus(col, row + 1), 0);
+                // setTimeout(() => this.moveFocus(col, row + 1), 0);
+                this.moveFocus(col, row + 1)
               }
             }
             break;
         }
       } else{
-        if (event.ctrlKey) {
           switch (event.key) {
             case 'Backspace':
-            cell.value = event.key;
             if (inputElement) {
+              console.log(inputElement.value);
               this.renderer.setProperty(inputElement, 'value', '');
+              setTimeout(() => this.moveFocus(col, row), 0);
+              console.log(inputElement.value);
+
             }
-        }
         }
       }
   }
@@ -155,11 +158,11 @@ export class GridComponent {
 
       console.log(col, row);
 
-      const cell = document.querySelector(`app-grid-cell[data-col="${col}"][data-row="${row}"] input`);
-      console.log(cell);
+      const cellInput = document.querySelector(`app-grid-cell[data-col="${col}"][data-row="${row}"] input`);
+      console.log(cellInput);
 
-      if (cell) {
-        (cell as HTMLElement).focus();
+      if (cellInput) {
+        setTimeout(() => (cellInput as HTMLElement).focus());
       }
     }
   }
